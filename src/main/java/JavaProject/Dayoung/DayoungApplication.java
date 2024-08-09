68a56e5aeff0d0ebe27f4543bf4a546c18594956
tae.y <0211ilyoil@gmail.com>
2024-08-09 11:28:37 +0900

    🌟 :: project setting

---
+++ src/main/java/JavaProject/Dayoung/DayoungApplication.java
@@ -0,0 +1,13 @@
+package JavaProject.Dayoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayoungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaProject/Dayoung/DayoungApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaProject.Dayoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
