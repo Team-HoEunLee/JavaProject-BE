e95a4582bb3cdc2fd7e39ac2a5920b223064a8f1
tae.y <0211ilyoil@gmail.com>
2024-08-11 20:52:11 +0900

    🌧 :: 토큰 재발급 api 추가

---
+++ src/main/java/JavaProject/Dayoung/domain/user/service/ReissueService.java
@@ -0,0 +1,18 @@
+package JavaProject.Dayoung.domain.user.service;
+
+import JavaProject.Dayoung.global.security.TokenResponse;
+import JavaProject.Dayoung.global.security.jwt.JwtReissueUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+@Service
+@RequiredArgsConstructor
+public class ReissueService {
+
+    private final JwtReissueUtil jwtReissueUtil;
+
+    @Transactional
+    public TokenResponse reissue(String refreshToken) {
+        return jwtReissueUtil.reissue(refreshToken);
+    }
+}
