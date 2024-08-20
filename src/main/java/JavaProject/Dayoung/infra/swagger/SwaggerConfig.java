2e1fbc7035a7e9239070478e39b8148a5db4bb3c
tae.y <0211ilyoil@gmail.com>
2024-08-20 09:09:55 +0900

    setting :: yml 설정

--- src/main/java/JavaProject/Dayoung/infra/swagger/SwaggerConfig.java
+++ src/main/java/JavaProject/Dayoung/infra/swagger/SwaggerConfig.java
@@ -3,7 +3,7 @@ package JavaProject.Dayoung.infra.swagger;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-
+//#http://localhost:8080/swagger-ui/index.html
 @OpenAPIDefinition(
         info =
         @Info(
