09c83e239dbd49dc3ca64350085ce27bf55cd64d
김가은 <kge02160@gmail.com>
2024-08-21 19:49:30 +0900

    chore :: Major enum 추가

---
+++ src/main/java/JavaProject/Dayoung/domain/user/entity/type/Major.java
@@ -0,0 +1,21 @@
+package JavaProject.Dayoung.domain.user.entity.type;
+
+public enum Major {
+    BACKEND,
+    FRONTEND,
+    IOS,
+    ANDROID,
+    FLUTTER,
+    GAME,
+    DEVOPS,
+    SECURITY,
+    AI,
+    DESIGN,
+    EMBEDED,
+    ETC,
+    DB,
+    OS,
+    DS,
+    NETWORK,
+    CA
+}
