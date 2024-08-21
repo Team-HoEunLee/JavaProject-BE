e347e9d7d0c1a2b456ae48c2447d1673eb29e6e5
tae.y <0211ilyoil@gmail.com>
2024-08-21 16:57:13 +0900

    feat :: code 엔티티 매핑

---
+++ src/main/java/JavaProject/Dayoung/domain/code/repository/CodeRepository.java
@@ -0,0 +1,7 @@
+package JavaProject.Dayoung.domain.code.repository;
+
+import JavaProject.Dayoung.domain.code.entity.Code;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CodeRepository extends CrudRepository<Code, Long> {
+}
