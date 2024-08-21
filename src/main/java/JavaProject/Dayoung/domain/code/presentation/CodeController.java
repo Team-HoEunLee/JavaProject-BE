f27cb150233a9004cbdbd9d6f8462dee88406754
tae.y <0211ilyoil@gmail.com>
2024-08-21 16:58:08 +0900

    feat :: createCode 추가

---
+++ src/main/java/JavaProject/Dayoung/domain/code/presentation/CodeController.java
@@ -0,0 +1,24 @@
+package JavaProject.Dayoung.domain.code.presentation;
+
+import JavaProject.Dayoung.domain.code.presentation.dto.request.CreateCodeRequest;
+import JavaProject.Dayoung.domain.code.service.CreateCodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/code")
+@RestController
+public class CodeController {
+
+    private final CreateCodeService createCodeService;
+
+    @PostMapping
+    public void createCode(@RequestBody @Valid CreateCodeRequest createQuizRequest) {
+        createCodeService.execute(createQuizRequest);
+    }
+}
