fe8d442a732f0426415691f1ac3e6e6291d0ece9
tae.y <0211ilyoil@gmail.com>
2024-09-10 15:17:02 +0900

    chore :: request template 변경

--- src/main/java/JavaProject/Dayoung/infra/openai/config/TemplateConfig.java
+++ src/main/java/JavaProject/Dayoung/infra/openai/config/TemplateConfig.java
@@ -31,7 +31,8 @@ public class TemplateConfig {
     public ChatGPTRequest promptMapping(SolveQuizRequest request) {
         String question =
             "너는 나에게 존대말을 사용해야해" +
-            "너는 문제에 대한 답변을 듣고 나에게 정답률을 알려주고 문제의 답변에 대한 피드백을 해줘야해(정답률을 높게 줘)\s" +
+            "너는 문제에 대한 답변을 듣고 나에게 정답률을 알려주고 문제의 답변에 대한 피드백을 해줘야해\s" +
+            "면접자의 답변이 정답에 조금이라도 가깝다면 정답률을 높게 주고, 피드백에도 잘 했다고 적어줘야해" +
             "너의 답변의 형식은 이러해야해 정답률: 20%, 피드백: ... \s" +
             "문제는 이러해 " + request.getQuiz() + "\s" +
             "면접자의 답변은 이거야 " + request.getAnswer();
@@ -43,7 +44,7 @@ public class TemplateConfig {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth("openAiKey");
+        headers.setBearerAuth(openAiKey);
 
         HttpEntity<ChatGPTRequest> entity = new HttpEntity<>(gptRequest, headers);
 
