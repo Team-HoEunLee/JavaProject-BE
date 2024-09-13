6e8754a8c88f95af022d073bcc27ec17eb7c2fea
김가은 <kge02160@gmail.com>
2024-09-13 11:23:45 +0900

    feat :: Area 생성 api 추가

--- src/main/java/JavaProject/Dayoung/domain/area/entity/Area.java
+++ src/main/java/JavaProject/Dayoung/domain/area/entity/Area.java
@@ -1,21 +1,22 @@
 package JavaProject.Dayoung.domain.area.entity;
 
 import JavaProject.Dayoung.domain.quiz.entity.Quiz;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Area {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "varchar(15)")
     private String areaName;
 
     @ManyToOne(fetch = FetchType.LAZY)
