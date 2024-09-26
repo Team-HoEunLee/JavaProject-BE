dd8024c2b5454eff26a4d503e8027658329412b9
tae.y <0211ilyoil@gmail.com>
2024-09-26 16:43:51 +0900

    ref :: 사용하지 않는 메서드 삭제

--- src/main/java/JavaProject/Dayoung/domain/quiz/repository/SolvedQuizRepositoryImpl.java
+++ src/main/java/JavaProject/Dayoung/domain/quiz/repository/SolvedQuizRepositoryImpl.java
@@ -1,38 +1,11 @@
 package JavaProject.Dayoung.domain.quiz.repository;
 
-import JavaProject.Dayoung.domain.quiz.entity.SolvedQuiz;
-import JavaProject.Dayoung.domain.quiz.entity.type.IsSolved;
 import JavaProject.Dayoung.domain.quiz.repository.port.SolvedQuizPort;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-import static JavaProject.Dayoung.domain.quiz.entity.QSolvedQuiz.solvedQuiz;
 
 @Repository
 @RequiredArgsConstructor
 public class SolvedQuizRepositoryImpl implements SolvedQuizPort {
 
-    private final JPAQueryFactory queryFactory;
-
-    @Override
-    public List<SolvedQuiz> queryAllById(Long id) {
-        return queryFactory
-            .selectFrom(solvedQuiz)
-            .where(solvedQuiz.id.eq(id))
-            .fetch();
-    }
-
-    @Override
-    public List<SolvedQuiz> queryAllByUserIdAndIsSolved(Long userId, IsSolved isSolved) {
-        return queryFactory
-            .selectFrom(solvedQuiz)
-            .where(
-                solvedQuiz.user.id.eq(userId),
-                solvedQuiz.isSolved.eq(isSolved)
-                )
-            .fetch();
-    }
 }
--- src/main/java/JavaProject/Dayoung/domain/quiz/repository/port/SolvedQuizPort.java
+++ src/main/java/JavaProject/Dayoung/domain/quiz/repository/port/SolvedQuizPort.java
@@ -1,14 +1,5 @@
 package JavaProject.Dayoung.domain.quiz.repository.port;
 
-import JavaProject.Dayoung.domain.quiz.entity.SolvedQuiz;
-import JavaProject.Dayoung.domain.quiz.entity.type.IsSolved;
-
-import java.util.List;
-
 public interface SolvedQuizPort {
-
-    List<SolvedQuiz> queryAllById(Long id);
-
-    List<SolvedQuiz> queryAllByUserIdAndIsSolved(Long userId, IsSolved isSolved);
 
 }
