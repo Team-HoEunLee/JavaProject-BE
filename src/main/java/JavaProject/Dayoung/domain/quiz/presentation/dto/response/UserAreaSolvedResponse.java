f507fb2d8c58b51a18210091f1556e95ea6828ff
김가은 <kge02160@gmail.com>
2024-11-01 12:22:48 +0900

    chore :: 유효성 검증 어노테이션 추가

--- src/main/java/JavaProject/Dayoung/domain/quiz/presentation/dto/response/UserAreaSolvedResponse.java
+++ src/main/java/JavaProject/Dayoung/domain/quiz/presentation/dto/response/UserAreaSolvedResponse.java
@@ -5,12 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class UserAreaSolvedResponse {
 
+    @NotNull(message = "영역은 null일 수 없습니다")
     private Area area;
 
+    @NotNull(message = "해결한 문제 수는 null일 수 없습니다")
+    @PositiveOrZero(message = "해결한 문제 수는 0 이상이어야 합니다")
     private Long solvedCount;
 }
