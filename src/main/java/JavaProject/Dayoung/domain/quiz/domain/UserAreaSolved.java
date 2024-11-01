6dcc5690ba8589c58ec1291547933dce5d8eaabd
김가은 <kge02160@gmail.com>
2024-11-01 12:16:30 +0900

    docs :: UserAreaSolved 설명

--- src/main/java/JavaProject/Dayoung/domain/quiz/domain/UserAreaSolved.java
+++ src/main/java/JavaProject/Dayoung/domain/quiz/domain/UserAreaSolved.java
@@ -6,6 +6,12 @@ import lombok.*;
 
 import javax.persistence.*;
 
+/*
+    사용자별 푼 문제의 분야를 통계, 관리하는 엔티티
+
+    각 사용자가 특정 분야에서 해결한 문제의 수를 추적합니다.
+    User와 Area 엔티티와의 다대일 관계를 통해 통계 정보를 저장합니다.
+ */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
